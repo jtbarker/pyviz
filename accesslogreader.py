c269fbc8cef1465e16600ed87d909ec4e3734ef3
Jon Barker <jonathan.t.barker@gmail.com>
2019-04-21 14:18:33 +0900

    added some histogram attempted plots

--- accesslogreader.py
+++ accesslogreader.py
@@ -11,6 +11,11 @@ date=pd.Series(data0[0])
 cleanedlatency = [x for x in latency if str(x) != 'nan']
 cleaneddate = [x for x in date if str(x) != 'nan']
 
+df = pd.DataFrame({
+'latencies': cleanedlatency,
+'dates': cleaneddate
+})
+hist = df.hist(bins=3)
 
 
 
@@ -20,7 +25,7 @@ cleaneddate = [x for x in date if str(x) != 'nan']
 #         x.append(int(row[0]))
 #         y.append(int(row[1]))
 
-plt.plot(latency,date)
+plt.plot(cleanedlatency,cleaneddate)
 plt.xlabel('latency')
 plt.ylabel('date')
 plt.title('Latency distribution\n Useful for logfile analysis')
