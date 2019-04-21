c236706efae975ae1242097089e50d4b6a9a158a
Jon Barker <jonathan.t.barker@gmail.com>
2019-04-21 14:00:42 +0900

    added some plots and a pandas dataframe reader

--- accesslogreader.py
+++ accesslogreader.py
@@ -1,18 +1,25 @@
 import matplotlib.pyplot as plt
 import csv
+import pandas as pd
 
-x = []
-y = []
 
-with open('access.log','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append(int(row[0]))
-        y.append(int(row[1]))
 
-plt.plot(x,y, label='Loaded from file!')
-plt.xlabel('x')
-plt.ylabel('y')
+data0=pd.read_csv('access.log',header=None, delimiter='\t')
+latency=pd.Series(data0[11])
+date=pd.Series(data0[0])
+
+
+
+
+# with open('access.log','r') as csvfile:
+#     plots = csv.reader(csvfile, delimiter='\t')
+#     for row in plots:
+#         x.append(int(row[0]))
+#         y.append(int(row[1]))
+
+plt.plot(latency,date)
+plt.xlabel('latency')
+plt.ylabel('date')
 plt.title('Interesting Graph\nCheck it out')
 plt.legend()
 plt.show()
